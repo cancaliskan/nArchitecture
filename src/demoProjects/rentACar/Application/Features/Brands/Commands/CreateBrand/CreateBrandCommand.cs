4a52cbb4b3a4a7049f87e24ec948d79295f811fc
DESKTOP-UQ2QC1I\canca <cancaliskan@windowslive.com>
2022-09-10 23:15:01 +0300

    Business Rule & global exceptions

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,37 @@
+﻿using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.CreateBrand;
+
+public class CreateBrandCommand : IRequest<CreatedBrandDto>
+{
+    public string Name { get; set; }
+
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandDto>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+        private readonly BrandBusinessRules _brandBusinessRules;
+
+        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper, BrandBusinessRules brandBusinessRule)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+            _brandBusinessRules = brandBusinessRule;
+        }
+
+        public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            await _brandBusinessRules.BrandNameCannotBeDuplicatedInserted(request.Name);
+
+            var mappedBrand = _mapper.Map<Brand>(request);
+            var createdBrand = await _brandRepository.AddAsync(mappedBrand);
+            var createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand);
+            return createdBrandDto;
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -0,0 +1,23 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+
+namespace Application.Features.Brands.Rules;
+
+public class BrandBusinessRules
+{
+    private readonly IBrandRepository _brandRepository;
+
+    public BrandBusinessRules(IBrandRepository brandRepository)
+    {
+        _brandRepository = brandRepository;
+    }
+
+    public async Task BrandNameCannotBeDuplicatedInserted(string name)
+    {
+        var result = await _brandRepository.GetListAsync(brand => brand.Name == name);
+        if (result.Items.Any())
+        {
+            throw new BusinessException("Brand name already exists");
+        }
+    }
+}
